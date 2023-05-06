db8a45fde74ca0a1672c11b101310cfeb80e3863
npkk <kob4.nemu@gmail.com>
2023-05-06 05:41:12 +0000

    fix: fix layout and click behavior

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,51 +1,49 @@
 <template>
   <div>
     <v-app>
-      <v-app-bar color="white" elevation="0">
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer">
-        </v-app-bar-nav-icon>
-        <v-app-bar-title>
-          <NuxtLink event="" @click.native="navigatoTo('/')">
-            npkk
-          </NuxtLink>
-          <v-btn to="/" :ripple="false" :variant="text">
-            <div class="appbar-title">
-              npkk
-            </div>
-          </v-btn>
-        </v-app-bar-title>
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" absolute bottom>
-        <v-list>
-          <v-list-item link to="/posts">
-            <v-list-item-title >
-              posts
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item link to="/about">
-            <v-list-item-title >
-              about
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
       <v-main>
-        <v-container>
-          <slot />
-        </v-container>
+        <v-app-bar color="white" elevation="0">
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+          </v-app-bar-nav-icon>
+          <v-app-bar-title class="d-flex">
+            <div class="appbar-title" @click.native="navigateTo('/')">
+              限界ブログ
+            </div>
+            <v-spacer />
+          </v-app-bar-title>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer">
+          <v-list>
+            <v-list-item link to="/posts">
+              <v-list-item-title>
+                posts
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item link to="/about">
+              <v-list-item-title>
+                about
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-layout>
+          <v-container class="ma-6">
+            <slot />
+          </v-container>
+        </v-layout>
       </v-main>
     </v-app>
   </div>
 </template>
 
 <script setup>
-import { NuxtLink } from '~/.nuxt/components';
-
 const drawer = useState('drawer', () => false)
 </script>
 
 <style scoped>
-.appbar-title{
-  font-size: 1.5em;
+.appbar-title {
+  font-size: 1.2em;
+  font-weight: 600;
+  cursor: pointer;
 }
 </style>
